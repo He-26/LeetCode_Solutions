9fb2e9eddd8292fed0803c32d0a7aceec56a66e7
Gireesh Pitchuka <62883238+He-26@users.noreply.github.com>
2023-02-24 07:44:15 +0530

    Create 24_Minimize_Deviation_in_Array.java

---
+++ Feb/24_Minimize_Deviation_in_Array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minimumDeviation(int[] nums) {
+        TreeSet<Integer> set=new TreeSet<>();
+        for(int i:nums){
+            if(i%2==1){
+                set.add(i*2);
+            }else{
+                set.add(i);
+            }
+        }
+        int diff=Integer.MAX_VALUE;
+        while(true){
+            int max=set.last();
+            int min=set.first();
+            int currDiff=max-min;
+            diff=Math.min(diff,currDiff);
+            if(max%2==0){
+                set.remove(max);
+                set.add(max/2);
+            }else{
+                return diff;
+            }
+        }
+    }
+}
