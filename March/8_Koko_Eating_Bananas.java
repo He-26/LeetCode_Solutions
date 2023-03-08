5d5b4d92b262ca06f401290bd5d9f0a52a357a34
Gireesh Pitchuka <62883238+He-26@users.noreply.github.com>
2023-03-08 08:17:44 +0530

    Create 8_Koko_Eating_Bananas.java

---
+++ March/8_Koko_Eating_Bananas.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int low=1,high=1_000_000_000;
+        while(low<high){
+            int mid=low+(high-low)/2;
+            System.out.print(mid+" ");
+            if(isPossible(piles,h,mid))
+                high=mid;
+            else
+                low=mid+1;
+        }
+        return low;
+    }
+    public boolean isPossible(int[] piles, int h, int k){
+        int time=0;
+        for(int p:piles){
+            time+=(p-1)/k+1;
+            if(time>h)  break;
+        }
+        return time<=h;
+    }
+}
